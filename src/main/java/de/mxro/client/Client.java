9f79e5b7cf824a3c5fd5d570856e71d028615d14
mxro <mxro@nowhere.com>
2015-01-15 11:04:01 +1100

    Devloping first implementation of client-api Autocommit Thu Jan 15 11:04:01 EST 2015

--- src/main/java/de/mxro/client/Client.java
+++ src/main/java/de/mxro/client/Client.java
@@ -1,5 +1,19 @@
 package de.mxro.client;
 
+import de.mxro.async.properties.PropertyNode;
+import de.mxro.factories.FactoryCollection;
+import de.mxro.service.ServiceRegistry;
+
 public interface Client {
+
+    public FactoryCollection factories();
+
+    public ServiceRegistry services();
+
+    public PropertyNode metrics();
+
+    public PropertyNode state();
+
+    public PropertyNode logs();
 
 }
